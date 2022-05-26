4bc0123b27a7ebe65aa6b4c9723c19a8f45a52d5
Gurjit Singh1 <gurjit.singh1@ibm.com>
2022-05-26 14:42:04 +0530

    day 6 and 7 code

--- force-app/main/default/lwc/contactListModal/contactListModal.js
+++ force-app/main/default/lwc/contactListModal/contactListModal.js
@@ -4,6 +4,11 @@ export default class ContactListModal extends LightningElement {
 
     @api contactsArray = []
 
+    connectedCallback() {
+        //contactsArray would be availabel here.
+        // but whenever any change occuers in contactsArray that won't be availabel here.
+    }
+
     handleClose() {
         const event = new CustomEvent('close')
         this.dispatchEvent(event)
