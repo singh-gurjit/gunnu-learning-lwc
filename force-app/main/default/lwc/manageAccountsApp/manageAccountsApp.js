1863dfcc10e4c8c15cce6c93a6884feea04533df
Gurjit Singh1 <gurjit.singh1@ibm.com>
2022-06-03 23:57:10 +0530

    day 10 changes

--- force-app/main/default/lwc/manageAccountsApp/manageAccountsApp.js
+++ force-app/main/default/lwc/manageAccountsApp/manageAccountsApp.js
@@ -6,14 +6,23 @@ export default class ManageAccountsApp extends LightningElement {
     showManageAccountsNotificationModal
     showManageAccountsNewAccount
     @track accountsArray = []
+    @track accountsArrayToDisplay = []
 
     @wire(getAccountList)
     handleAccountList(result) {
 
         if (result.data) {
             this.accountsArray = JSON.parse(JSON.stringify(result.data))
+            this.accountsArrayToDisplay = JSON.parse(JSON.stringify(result.data))
         } else if (result.error) {
             console.error('There is an error', result.error)
         }
+    }
+
+    handleSearchChange(event) {
+        let inputValue = event.detail
+        inputValue = inputValue.toLowerCase()
+        this.accountsArrayToDisplay = this.accountsArray.filter(data => data.Name.toLowerCase().includes(inputValue))
+
     }
 }
--- force-app/main/default/lwc/manageAccountsHeader/manageAccountsHeader.js
+++ force-app/main/default/lwc/manageAccountsHeader/manageAccountsHeader.js
@@ -1,3 +1,10 @@
 import { LightningElement } from 'lwc';
 
-export default class ManageAccountsHeader extends LightningElement {}
+export default class ManageAccountsHeader extends LightningElement {
+
+    handleSearchChange(event) {
+        this.dispatchEvent(new CustomEvent('searchchange', {
+            detail: event.target.value
+        }))
+    }
+}
